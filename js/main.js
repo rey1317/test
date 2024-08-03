228320a5b9e36ec63bd1e152fe3b1c1bb91258ae
zhukovbro <azhykov@gmail.com>
2024-08-03 15:19:55 +0200

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 import '../css/style.css'
 import '../css/media.css'
-import avatar from '/avatar.svg'
-
+import avatar from '/img/avatar.svg'
 
 document.querySelector('#app').innerHTML = `
   <div class="container">
@@ -9,32 +8,32 @@ document.querySelector('#app').innerHTML = `
       <div class="row-one__left">
         <img src="${avatar}" class="row-one__left__avatar" alt="">
         <div class="row-one__left__info block-grey-radius">
-          <div class="row-one__left__info__line-top">
+          <div class="row-one__left__info__line-top" contenteditable="true">
             Hello 👋🏻 I’m
           </div>
           <div class="row-one__left__info__line-bottom">
-            <div class="row-one__left__info__line-bottom__name">
+            <div class="row-one__left__info__line-bottom__name" contenteditable="true" data-key="name">
               Karthik SR
             </div>
-            <div class="row-one__left__info__line-bottom__job">
+            <div class="row-one__left__info__line-bottom__job" contenteditable="true">
               UX/UI Designer
             </div>
           </div>
         </div>
       </div> 
-      <div class="row-one__right block-grey-radius">
-        <div class="row-one__right__top">
+      <div class="row-one__right block-grey-radius ripple-container">
+        <div class="row-one__right__top" contenteditable="true">
           Languages
         </div>
         <div class="row-one__right__bottom">
           <div class="row-one__right__bottom__text">
-            <div class="row-one__right__bottom__text__row">
+            <div class="row-one__right__bottom__text__row" contenteditable="true" data-key="lang-1">
               English
             </div>
-            <div class="row-one__right__bottom__text__row">
-              Malayalam
+            <div class="row-one__right__bottom__text__row" contenteditable="true" data-key="lang-2">
+              Malayalamс
             </div>
-            <div class="row-one__right__bottom__text__row">
+            <div class="row-one__right__bottom__text__row" contenteditable="true" data-key="lang-3">
               Hindi
             </div>
           </div>
@@ -55,7 +54,8 @@ document.querySelector('#app').innerHTML = `
           <div class="row-two__left__experience__item">
             <div class="row-one__right__bottom__lines__item__top">
               <div class="row-two__left__experience__item__top">
-                Jun. 2023 - Present
+                Jun. 2023 - Present 
+                <span class="row-two__left__experience__item__top__fav">most recent</span>
               </div>
               <div class="row-two__left__experience__item__top__tag">
                 
@@ -137,25 +137,193 @@ document.querySelector('#app').innerHTML = `
           </div>
         </div>
       </div>
-      <div class="row-two__right block-grey-radius">
+      <div class="row-two__right block-grey-radius ripple-container">
         <div class="row-two__right__title">
           Tools
         </div>
         <div class="row-two__right__inner">
-          
+          <div class="row-two__right__inner__block">
+            <span class="row-two__right__inner__block__overlay">design</span>
+            <img src="/img/tools_logo/figma.svg" alt="Figma">
+            <img src="/img/tools_logo/ps.svg" alt="Photoshop">
+            <img src="/img/tools_logo/ai.svg" alt="Illustrator">
+            <img src="/img/tools_logo/pr.svg" alt="Premiere">
+            <img src="/img/tools_logo/notion.svg" alt="Notion">
+            <img src="/img/tools_logo/meets.svg" alt="Meets">
+          </div>
+          <div class="row-two__right__inner__block">
+              <span class="row-two__right__inner__block__overlay">no-code</span>
+              <img src="/img/tools_logo/zapier.svg" alt="Zapier">
+              <img src="/img/tools_logo/webflow.svg" alt="Webflow">
+              <img src="/img/tools_logo/framer.svg" alt="Framer">
+              <img src="/img/tools_logo/wp.svg" alt="WordPress">
+          </div>
+          <div class="row-two__right__inner__block">
+              <span class="row-two__right__inner__block__overlay">artificial intelligence</span>
+              <img src="/img/tools_logo/gpt.svg" alt="GPT">
+              <img src="/img/tools_logo/copilot.svg" alt="GitHub Copilot">
+              <img src="/img/tools_logo/mj.svg" alt="MidJourney">
+              <!-- <img src="/img/tools_logo/mj3.svg" alt="MidJourney v3"> -->
+          </div>
         </div>
       </div>
     </div>
     <div class="row-three">
       <div class="row-three__left block-grey-radius">
-        asdf
+        <div class="row-three__left__title">
+          Education
+        </div>
+        <div class="row-three__left__inner">
+          <div class="row-three__left__inner__item">
+            <div class="row-three__left__inner__item__year">
+              2023<img src="/img/heart.svg" alt="">
+            </div>
+            <div class="row-three__left__inner__item__direction">
+              <div class="row-three__left__inner__item__direction__title">
+                UI/UX
+              </div>
+              <div class="row-three__left__inner__item__direction__tags">
+                <div class="row-three__left__inner__item__direction__tags__item">
+                  #UX
+                </div>
+                <div class="row-three__left__inner__item__direction__tags__item">
+                  #UI
+                </div>
+                <div class="row-three__left__inner__item__direction__tags__item">
+                  #research
+                </div>
+                <div class="row-three__left__inner__item__direction__tags__item">
+                  #DesignSystem
+                </div>
+                <div class="row-three__left__inner__item__direction__tags__item">
+                  #Ui
+                </div>
+                <div class="row-three__left__inner__item__direction__tags__item">
+                  #wireframing
+                </div>
+              </div>
+            </div>
+            <div class="row-three__left__inner__item__school">
+              Coursera
+            </div>
+          </div>
+
+          <div class="row-three__left__inner__item">
+            <div class="row-three__left__inner__item__year">
+              2023
+            </div>
+            <div class="row-three__left__inner__item__direction">
+              <div class="row-three__left__inner__item__direction__title">
+                UI/UX
+              </div>
+              <div class="row-three__left__inner__item__direction__tags">
+                <div class="row-three__left__inner__item__direction__tags__item">
+                  #UX
+                </div>
+                <div class="row-three__left__inner__item__direction__tags__item">
+                  #UI
+                </div>
+                <div class="row-three__left__inner__item__direction__tags__item">
+                  #research
+                </div>
+                <div class="row-three__left__inner__item__direction__tags__item">
+                  #DesignSystem
+                </div>
+                <div class="row-three__left__inner__item__direction__tags__item">
+                  #Ui
+                </div>
+                <div class="row-three__left__inner__item__direction__tags__item">
+                  #wireframing
+                </div>
+              </div>
+            </div>
+            <div class="row-three__left__inner__item__school">
+              Coursera
+            </div>
+          </div>
+
+          <div class="row-three__left__inner__item">
+            <div class="row-three__left__inner__item__year">
+              2023
+            </div>
+            <div class="row-three__left__inner__item__direction">
+              <div class="row-three__left__inner__item__direction__title">
+                UI/UX
+              </div>
+              <div class="row-three__left__inner__item__direction__tags">
+                <div class="row-three__left__inner__item__direction__tags__item">
+                  #UX
+                </div>
+                <div class="row-three__left__inner__item__direction__tags__item">
+                  #UI
+                </div>
+                <div class="row-three__left__inner__item__direction__tags__item">
+                  #research
+                </div>
+                <div class="row-three__left__inner__item__direction__tags__item">
+                  #DesignSystem
+                </div>
+                <div class="row-three__left__inner__item__direction__tags__item">
+                  #Ui
+                </div>
+                <div class="row-three__left__inner__item__direction__tags__item">
+                  #wireframing
+                </div>
+              </div>
+            </div>
+            <div class="row-three__left__inner__item__school">
+              Coursera
+            </div>
+          </div>
+
+
+        </div>
       </div>
       <div class="row-three__right">
         <div class="row-three__right__top block-grey-radius">
-          asdf
+          <div class="row-three__right__top__title">
+            Interests
+          </div>
+          <div class="row-three__right__top__tags">
+            <div class="row-three__right__top__tags__item">
+              branding
+            </div>
+            <div class="row-three__right__top__tags__item">
+              design
+            </div>
+            <div class="row-three__right__top__tags__item">
+              photography
+            </div>
+            <div class="row-three__right__top__tags__item">
+              artificial intelligence
+            </div>
+            <div class="row-three__right__top__tags__item">
+              illustration
+            </div>
+            <div class="row-three__right__top__tags__item">
+              typography
+            </div>
+            <div class="row-three__right__top__tags__item">
+              social networks
+            </div>
+            <div class="row-three__right__top__tags__item">
+              research
+            </div>
+             <div class="row-three__right__top__tags__item">
+              dron pilot
+            </div>
+            <div class="row-three__right__top__tags__item">
+              games
+            </div>
+          </div>
         </div>
         <div class="row-three__right_bottom">
-          asdf
+          <div class="row-three__right_bottom__title">
+            Let´s chat! I´m ready to work on excinting projects
+          </div>
+          <div class="row-three__right_bottom__email">
+            srkarthik.designscape@gmail.com
+          </div>
         </div>
       </div>
     </div>
@@ -163,11 +331,64 @@ document.querySelector('#app').innerHTML = `
   </div>
 `
 
+document.querySelectorAll('.ripple-container').forEach(element => {
+  element.addEventListener('click', function(e) {
+    const ripple = document.createElement('span');
+    ripple.className = 'ripple';
+    const rect = element.getBoundingClientRect();
+    ripple.style.width = ripple.style.height = Math.max(rect.width, rect.height) + 'px';
+    ripple.style.left = e.clientX - rect.left - ripple.offsetWidth / 2 + 'px';
+    ripple.style.top = e.clientY - rect.top - ripple.offsetHeight / 2 + 'px';
+    element.appendChild(ripple);
+    setTimeout(() => ripple.remove(), 600); // Убираем ripple элемент после анимации
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Save data to localStorage on input event
+  document.querySelectorAll('[contenteditable="true"]').forEach(element => {
+    const key = element.getAttribute('data-key'); // Use the data-key attribute as key
+    if (!key) {
+      console.error('Element does not have a data-key:', element);
+      return;
+    }
+
+    console.log(`Loading data for ${key}`);
+    const savedData = localStorage.getItem(key);
+    if (savedData) {
+      element.innerText = savedData;
+      console.log(`Loaded data for ${key}:`, savedData);
+    }
+
+    element.addEventListener('input', () => {
+      console.log(`Saving data for ${key}:`, element.innerText);
+      localStorage.setItem(key, element.innerText);
+    });
+  });
+
+  // Add event listener to the button
+  document.getElementById('download-pdf').addEventListener('click', async () => {
+    downloadPDF();
+  });
+});
 
 
+// function downloadPDF() {
+//     const element = document.getElementById('app');
+
+//     html2pdf().from(element).save('resume.pdf'); // Используйте html2pdf.js библиотеку
+// }
 
 
+function downloadPDF() {
+  const element = document.getElementById('app');
+  const opt = {
+    margin:       1,
+    filename:     'resume.pdf',
+    image:        { type: 'jpeg', quality: 0.98 },
+    html2canvas:  { scale: 2, useCORS: true },
+    jsPDF:        { unit: 'pt', format: 'a4', orientation: 'portrait' }
+  };
 
-
-
-
+  html2pdf().from(element).set(opt).save();
+}
---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/test/', // Указывайте корректный путь в соответствии с именем вашего репозитория на GitHub
+  // Дополнительные настройки сборки
+});
